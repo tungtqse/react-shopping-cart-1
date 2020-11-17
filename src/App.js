1d3ef612ea22bf7ca475b2d590df50d87a914c51
Tung Tran <tungtqse02084@gmail.com>
2020-11-17 22:23:29 +0800

    product component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from "react";
+import data from "./data.json"
+import Products from './components/Products';
  
-function App() {
-  return(
-    <div className="grid-container">
-      <header>
-        <a href="/">Shopping Cart</a>
-      </header>
-      <main>
-        Product List
-      </main>
-      <footer>
-        About
-      </footer>
-    </div>
-  )
+class App extends Component {
+
+  constructor(){
+    super();
+    
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: ""
+    }
+  }
+
+  render(){
+    return(
+      <div className="grid-container">
+        <header>
+          <a href="/">Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}></Products>
+            </div>
+            <div className="sidebar">
+              Cart
+            </div>
+          </div>
+        </main>
+        <footer>
+          About
+        </footer>
+      </div>
+    )
+  }  
 }
  
 export default App;
